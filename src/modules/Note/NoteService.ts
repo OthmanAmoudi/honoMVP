229b648f16e952c248171e23c76bf83bda6eeb2c
OthmanAmoudi <red.fantastic@hotmail.com>
2024-09-12 03:53:41 +0300

    merget temp branch with main for postgresql changes

--- src/modules/Note/NoteService.ts
+++ src/modules/Note/NoteService.ts
@@ -2,12 +2,12 @@
 import { NotFoundError, BaseService } from "../../utils";
 import { eq, gt, asc } from "drizzle-orm";
 import {
-  InsertNoteSchema,
-  UpdateNoteSchema,
-  NewNote,
-  notesTable,
-  UpdateNote,
   Note,
+  NewNote,
+  UpdateNote,
+  notesTable,
+  UpdateNoteSchema,
+  InsertNoteSchema,
 } from "./NoteModel";
 export default class NoteService extends BaseService {
   async getAll(cursor?: string, limit: number = 3): Promise<Note[]> {
@@ -37,26 +37,30 @@ export default class NoteService extends BaseService {
   async create(data: NewNote) {
     return this.handleErrors(async () => {
       const cleanedData = this.validate(InsertNoteSchema, data);
-      const result = await this.db
+      const createdId = await this.db
         .insert(notesTable)
         .values(cleanedData)
-        .returning();
-      return result[0];
+        .$returningId();
+      console.log(createdId);
+      const result = await this.getById(createdId[0].id);
+      // .returning();
+      return result;
     });
   }
 
   async update(id: string, data: UpdateNote) {
     return this.handleErrors(async () => {
       const cleanedData = this.validate(UpdateNoteSchema, data);
-      const result = await this.db
+      const updatedId = await this.db
         .update(notesTable)
         .set(cleanedData)
-        .where(eq(notesTable.id, id)) // convert to number if your id is a number e.g Number(id)
-        .returning();
-      if (!result[0]) {
+        .where(eq(notesTable.id, id)); // convert to number if your id is a number e.g Number(id)
+      // .returning();
+      if (!updatedId[0].affectedRows) {
         throw new NotFoundError(`Resource with id ${id} not found`);
       }
-      return result[0];
+      const result = await this.getById(id);
+      return result;
     });
   }
 
@@ -64,9 +68,9 @@ export default class NoteService extends BaseService {
     return this.handleErrors(async () => {
       const result = await this.db
         .delete(notesTable)
-        .where(eq(notesTable.id, id)) // convert to number if your id is a number e.g Number(id)
-        .returning();
-      if (!result[0]) {
+        .where(eq(notesTable.id, id)); // convert to number if your id is a number e.g Number(id)
+      // .returning();
+      if (!result[0].affectedRows) {
         throw new NotFoundError(`Resource with id ${id} not found`);
       }
     });
