6e7529a6423492d4fbebe40e28e5cad329a83884
OthmanAmoudi <red.fantastic@hotmail.com>
2024-09-17 16:48:47 +0300

    update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   schema: "./src/modules/**/**Model.ts",
-  out: "./drizzle",
+  out: "./src/db/migrations",
   dialect: "sqlite",
   dbCredentials: {
     url: "./sqlite.db",
